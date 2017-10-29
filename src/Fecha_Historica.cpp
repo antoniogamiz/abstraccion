3467e05afb31399b4a3008a37fbbd4b4fa533016
Elena Merelo <ElenaMerelo@users.noreply.github.com>
2017-10-29 17:21:43 +0100

    Minor changes
    Añado métodos Copiar, ReservarMemoria y LiberarMemoria para minimizar el código repetido.

--- src/Fecha_Historica.cpp
+++ src/Fecha_Historica.cpp
@@ -5,14 +5,33 @@
 using namespace std;
 
 //Resize simple
-  void Fecha_Historica::resize(int r){
-     string *aux = new string[r];   
+  void Fecha_Historica::Resize(int r){
+     string *aux = new string[r];
      for(int i=0; i<numeventos; ++i){
-        aux[i]=str[i]; 
+        aux[i]=str[i];
      }
      delete[] str;
      str = aux;
      reservados = r;
+  }
+
+  void Fecha_Historica::ReservarMemoria(int n){
+    assert(n>0);
+    reservados=n;
+    string *str = new string [n];
+  }
+
+  void Fecha_Historica::LiberarMemoria(){
+    anio=numeventos=reservados=0;
+    delete [] str;
+  }
+
+  void Fecha_Historica::Copiar(string *s, int reserv, int num_events){
+    assert(reserv >= num_events);
+    ReservarMemoria(reserv);
+    numeventos=num_events;
+    for(int i=0; i<num_events; i++)
+      str[i]=s[i];
   }
 
 //Constructor vacío
@@ -20,46 +39,41 @@ using namespace std;
 
 
 //Constructor con parametros año, cadena de eventos y número de eventos a añadir
-  Fecha_Historica::Fecha_Historica(int a, string *s, int n):reservados(n),numeventos(n){
+  Fecha_Historica::Fecha_Historica(int a, string *s, int n){
      assert(a >= 0 && a<= 9999);
      anio = a;
-     string *str = new string[n];   
-     for (int i=0; i<n; i++)
-        str[i]=s[i];
+     ReservarMemoria(n);
+     Copiar(s,n,n);
   }
 
-//Constructor de copias
-   Fecha_Historica::Fecha_Historica(const Fecha_Historica& e){
+//Constructor de copia
+   Fecha_Historica::Fecha_Historica(const Fecha_Historica &e){
     *this=e;
    }
 
-   Fecha_Historica& Fecha_Historica::operator=(const Fecha_Historica& e)
-   {
+   Fecha_Historica& Fecha_Historica::operator=(const Fecha_Historica &e){
        if(this != &e){
-        str = new string[e.reservados];
-        anio=e.anio;
-        numeventos = e.numeventos;
-        reservados = e.reservados;
-        for(int i=0; i<numeventos; ++i)
-           str[i] = e.str[i];
+         LiberarMemoria();
+         anio=e.anio;
+         Copiar(e.str,e.reservados,e.numeventos);
        }
        return *this;
    }
-   
-   
+
+
 //Método de acceso a año
-   int Fecha_Historica::getAnio(){
+   int Fecha_Historica::GetAnio(){
     return anio;
    }
 
 
 //Añade un evento
-  void Fecha_Historica::aniadirevento(string& event){
+  void Fecha_Historica::AddEvento(string &event){
      if (numeventos == reservados){
         if (numeventos==0)
-           resize(1);
+           Resize(1);
         else
-        resize(2*reservados);
+        Resize(2*reservados);
      }
      str[numeventos]=event;
      numeventos++;
@@ -71,14 +85,14 @@ using namespace std;
      bool encontrado=false;
      for (int i=0; i < numeventos; ++i){
         if(str[i].find(s) != -1 ){
-           matches.aniadirevento(str[i]);
+           matches.AddEvento(str[i]);
            encontrado = true;
         }
      }
      if (encontrado){
         matches.anio=anio;
       }
-    return encontrado;       
+    return encontrado;
    }
 
 //Operador <<
@@ -91,8 +105,7 @@ using namespace std;
   }
 
 //Operador >>
-  istream& operator>> (istream& is, Fecha_Historica& e)
-  {
+  istream& operator>> (istream& is, Fecha_Historica& e){
     string line; char c;
     is >> e.anio >> c;
     getline(is, line);
@@ -109,9 +122,9 @@ using namespace std;
     e.str=new string[n];
 
     stringstream ss(line);
-    
+
     for(int i=0; i<n; i++)
         getline(ss, e.str[i], '#');
     return is;
 
-  }  
+  }
